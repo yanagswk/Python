6cb8129f614a485098e3e0c6157b4dc23227bfec
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2020-12-06 15:59:40 +0900

    udemy_aplicationのコピーを削除

--- Python/udemy_aplicationのコピー/main.py
+++
@@ -1,6 +0,0 @@
-import roboter.controller.conversation
-
-roboter.controller.conversation.talk_about_restaurant()
-
-
-
--- Python/udemy_aplicationのコピー/settings_.py
+++
@@ -1,3 +0,0 @@
-# CSV_FILE_PATH = '/tmp/test.csv'
-# TEMPLATE_PATH = '/tmp/templates/'
-# TEMPLATE_PATH = None
