5a9cccc78b55bec9438b5b602e2fd8325268fefb
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2020-12-09 12:39:40 +0900

    APIファイル削除

--- Python/API/DeviceListApi/Django_api/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\yanag\iclouddrive\programing\python\api\devicelistapi\django_api\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
