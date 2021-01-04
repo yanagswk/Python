bef3408440833ffaa33c5877dd64978e37bc7dc1
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2021-01-04 12:31:26 +0900

    クラス化から関数へ変更

--- original_app/ramen/views/find_file.py
+++ original_app/ramen/views/find_file.py
@@ -5,51 +5,43 @@ import string
 
 import settings
 
+template_dir_path = settings.TEMPLATE_PATH
 
-class GetTemplate():
+
+def get_template_dir_path():
     """
-    templateを読み込むクラス
+    settingsからディレクトリのpathを返す。
     """
-
-    def __init__(self):
-        self.template_dir = self.get_template_dir_path()
-
-    def get_template_dir_path(self):
-        """
-        settingsからディレクトリのpathを返す。
-        """
-        template_dir_path = None
-        try:
-            import settings
-            template_dir_path = settings.TEMPLATE_PATH
-        except ImportError:
-            # TODO logger導入
-            print('settingsが存在しません。')
-
-        if not template_dir_path:
-            base_dir = os.path.dirname(
-                os.path.dirname(os.path.abspath(__file__)))
-            template_dir_path = os.path.join(base_dir, 'templates')
+    global template_dir_path
+    if template_dir_path:
         return template_dir_path
 
-    def find_template(self, txt_file):
-        """
-        ファイルが存在するかを確認。
-        """
-        # template_dir_path = get_template_dir_path()
-        template_file_path = os.path.join(self.template_dir, txt_file)
-        if not os.path.exists(template_file_path):
-            raise FileNotFoundError(f'{txt_file}が存在しません。')
-        return template_file_path
+    else:
+        base_dir = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))
+        template_dir_path = os.path.join(base_dir, 'templates')
+    return template_dir_path
 
-    def get_template(self, txt_file):
-        """
-        templateを読み込む。
-        """
-        template = self.find_template(txt_file)
-        with open(template, encoding='utf-8') as template_file:
-            read_template = template_file.read()
-            # read_template = read_template.rstrip('\n')
-            read_template = termcolor.colored(read_template,
-                                              settings.TEMPLATE_COLOR)
-            return string.Template(read_template)
+
+def find_template(txt_file):
+    """
+    ファイルが存在するかを確認。
+    """
+    template_dir_path = get_template_dir_path()
+    template_file_path = os.path.join(template_dir_path, txt_file)
+    if not os.path.exists(template_file_path):
+        raise FileNotFoundError(f'{txt_file}が存在しません。')
+    return template_file_path
+
+
+def get_template(txt_file):
+    """
+    templateを読み込む。
+    """
+    template = find_template(txt_file)
+    with open(template, encoding='utf-8') as template_file:
+        read_template = template_file.read()
+        # read_template = read_template.rstrip('\n')
+        read_template = termcolor.colored(read_template,
+                                          settings.TEMPLATE_COLOR)
+        return string.Template(read_template)
